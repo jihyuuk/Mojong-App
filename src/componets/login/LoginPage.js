5b51326c19b8faea06cddf992e57c97a9fcc08fb
jihyuuk_home <jihyuk.dev@gmail.com>
2024-02-07 07:13:26 +0900

    HTML/CSS : 로그인페이지 레이아웃 및 디자인 수정함

--- src/componets/login/LoginPage.js
+++ src/componets/login/LoginPage.js
@@ -87,31 +87,33 @@ function LonginPage() {
 
 
     return (
-        <div className='bg-body-secondary'>
+            <div className='d-flex align-items-center justify-content-center bg-white h-100'>
+                <div className='text-center p-5 pt-0' style={{maxWidth:'450px'}}>
 
-            <div className='text-center bg-white p-5 m-auto' style={{ maxWidth: '400px', height: '100%' }}>
-                <img src={process.env.PUBLIC_URL + '/logoTest.png'} className='w-50' />
-                <h3 className='my-4'>로그인</h3>
-                <Form noValidate onSubmit={handleSubmit}>
-                    <FloatingLabel controlId="loginId" label="직원명" className="mb-2">
-                        <Form.Control type="text" placeholder="홍길동" value={username} onChange={(e) => nameChange(e.target.value.trim())} isInvalid={isInValidName} />
-                        <Form.Control.Feedback type="invalid" className='text-start'>{feedbackName}</Form.Control.Feedback>
-                    </FloatingLabel>
+                    <img src={process.env.PUBLIC_URL + '/loginLogo.png'} className='w-100 px-3 mb-3' />
 
-                    <FloatingLabel controlId="loginPassword" label="비밀번호" className="mb-3">
-                        <Form.Control type="password" placeholder="비밀번호" value={password} onChange={(e) => pwdChange(e.target.value.trim())} isInvalid={isInValidPwd} />
-                        <Form.Control.Feedback type="invalid" className='text-start'>{feedbackPwd}</Form.Control.Feedback>
-                    </FloatingLabel>
+                    <Form noValidate onSubmit={handleSubmit} className='pt-4'>
+                        <p className='fs-4 fw-medium mb-3 text-start'>로그인</p>
+                        
+                        <FloatingLabel controlId="loginId" label="직원명" className="mb-3">
+                            <Form.Control type="text" placeholder="홍길동" value={username} onChange={(e) => nameChange(e.target.value.trim())} isInvalid={isInValidName} />
+                            <Form.Control.Feedback type="invalid" className='text-start'>{feedbackName}</Form.Control.Feedback>
+                        </FloatingLabel>
 
-                    <div className='d-flex justify-content-between'>
-                        <Form.Check type="switch" id="autoLogin" label="자동로그인" className='text-start mb-4' checked={autoLogin} onChange={() => setAutoLogin(!autoLogin)} />
-                        <Link to='/join'>회원가입</Link>
-                    </div>
-                    <Button type='submit' className='w-100 py-2'>로그인</Button>
-                </Form>
+                        <FloatingLabel controlId="loginPassword" label="비밀번호" className="mb-3">
+                            <Form.Control type="password" placeholder="비밀번호" value={password} onChange={(e) => pwdChange(e.target.value.trim())} isInvalid={isInValidPwd} />
+                            <Form.Control.Feedback type="invalid" className='text-start'>{feedbackPwd}</Form.Control.Feedback>
+                        </FloatingLabel>
+
+                        <div className='d-flex justify-content-between mb-3'>
+                            <Form.Check type="switch" id="autoLogin" label="자동로그인" className='text-start mb-4' checked={autoLogin} />
+                            <Link to='/join' className='text-success'>회원가입</Link>
+                        </div>
+                        <Button type='submit' variant="success" className='fs-5 fw-semibold w-100 mt-4 rounded-5 py-2'>로그인</Button>
+                    </Form>
+
+                </div>
             </div>
-
-        </div>
     );
 }
 
