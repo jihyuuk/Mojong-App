a3508131349abeb02e1bc04174eff606e4ee29db
jihyuuk_home <jihyuk.dev@gmail.com>
2024-02-08 10:10:39 +0900

    Refactor : 카테고리 대폭 수정, 벳지형태를 언더라인으로 바꿈

--- src/componets/home/category/Category.js
+++ src/componets/home/category/Category.js
@@ -1,51 +1,27 @@
-import React, { useContext } from 'react';
-import { Badge, Nav, Navbar, Stack } from 'react-bootstrap';
-import { Events, Link } from 'react-scroll';
+import React, { useContext, useState } from 'react';
 import { DataContext } from '../../../App';
 
-//바 스크롤중에 hadleSetActive동작 막기위함
-let targetSection = '';
-let isScrolling = false;
 
-//카테고리 클릭시 발생되는 이벤트
-Events.scrollEvent.register('begin', function (to, element) {
-    scrollNav(to + "-nav")
-    targetSection = to;
-    isScrolling = true;
-    console.log("스크롤시작됨 element")
-})
 
-//현재 시청 섹션 변경시 호출됨
-const handleSetActive = (to) => {
-    if (isScrolling) {
-        if (`${to}` === targetSection) {
-            isScrolling = false;
-            console.log("스크롤끝")
-        }
-        return;
-    }
-    console.log("hadleSetActive 호출됨")
-    scrollNav(`${to}-nav`)
-};
 
-//카테고리 바 스크롤
-const scrollNav = (sectionNav) => {
+function Category(props) {
 
-    const element = document.getElementById(sectionNav);
+    const datas = useContext(DataContext);
+    const selectedCategory = props.selectedCategory;
+    const setSelectedCategory = props.setSelectedCategory;
 
-    if (element) {
-        //이전 badge 의 active 지우기
-        const activeElements = document.getElementsByClassName("nav-active")
-        for (const activeElement of activeElements) {
-            activeElement.classList.remove('nav-active');
-        }
 
-        //선택한 badge에 actvie 부여하기
-        element.parentNode.classList.add("nav-active")
+    //카테고리 바 스크롤
+    const scrollNav = (e) => {
 
-        const container = document.getElementById('scroll-nav');
+        const element = e.target;
+
+        //현재 선택된 카테고리 적용
+        setSelectedCategory(e.target.textContent)
+
         //화면 중앙에 오게
-        const targetLeft = element.offsetLeft + (element.offsetWidth / 2) - (container.clientWidth / 2);
+        const container = document.getElementById('scroll-nav');
+        const targetLeft = element.offsetLeft + (element.offsetWidth  - container.clientWidth)/2;
 
         container.scrollTo({
             left: targetLeft,
@@ -53,27 +29,16 @@ const scrollNav = (sectionNav) => {
             ignoreCancelEvents: true
         });
     }
-}
-
-function Category() {
-
-    const datas = useContext(DataContext);
 
     return (
-        <div id="scroll-nav" className='overflow-auto border-bottom' >
-            <Navbar>
-                <Nav navbarScroll>
-                    <Stack direction="horizontal" gap={2} className='p-2 fs-1' id="scroll-nav-stack">
-
-                        {datas.map((data, index) => (
-                            <Badge key={index} pill bg="defalut" className='border'>
-                                <Link to={`section${index}`} offset={-200} spy={true} onSetActive={handleSetActive} ignoreCancelEvents={true} id={`section${index}-nav`}>{data.category}</Link>
-                            </Badge>
-                        ))}
-
-                    </Stack>
-                </Nav>
-            </Navbar>
+        <div id='scroll-nav' className='overflow-x-auto w-100 d-flex bg-white border-bottom fw-medium text-secondary shadow-sm'>
+                {datas.map((data, index) => (
+                    <div key={index} className={`py-2 px-3 fs-5 ${selectedCategory === data.category ? 'selected' : ''}`} onClick={(e) => scrollNav(e)}>
+                        <p className='text-nowrap'>
+                            {data.category}
+                        </p>
+                    </div>
+                ))}
         </div>
     )
 
