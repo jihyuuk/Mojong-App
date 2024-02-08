31e15e079ed9071224e759033b27f61a85aa842a
jihyuuk_home <jihyuk.dev@gmail.com>
2024-02-09 07:04:19 +0900

    Refactor : 홈 레이아웃 수정, 푸터추가함

--- src/componets/home/Home.js
+++ src/componets/home/Home.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Category from './category/Category';
 import Header from './header/Header';
 import Section from './section/Section';
 import GoToCart from './bottomBtn/GoToCart';
+import Footer from '../footer/Footer'
+import { DataContext } from '../../App';
 
 function Home() {
+
+    const data = useContext(DataContext);
+    const [selectedCategory, setSelectedCategory] = useState(data[0].category);
+    const [items, setItems] = useState(data[0].items)
+
+    //카테고리 바뀌면 내용 바꾸기
+    useEffect(() => {
+        const category = data.find((category) => category.category === selectedCategory);
+        setItems(category.items);
+    }, [selectedCategory]);
+
+
+    //높이설정
+    useEffect(() => { }, [])//처음 마운트 될때만 실행
+
     return (
-        <>
-            {/* 헤더 상단 */}
+        <div className='d-flex flex-column h-100 position-relative'>
+
+            {/* 헤더 */}
             <Header></Header>
 
+            {/* 카테고리 */}
+            <Category selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}></Category>
+
             {/* 센션*/}
-            <Section></Section>
+            <Section items={items}></Section>
 
             {/*하단 장바구니 버튼 */}
             <GoToCart></GoToCart>
+            
+            {/* 푸터 */}
+            <Footer value={'home'}></Footer>
 
-            {/* 푸터*/}
-            <footer style={{ height: 200 }}>
-            </footer>
-        </>
+        </div>
     );
 }
 
