5a4d24e70dcdeeadb6080a3bc7356cbcbf03adf7
jihyuuk_home <jihyuk.dev@gmail.com>
2024-02-08 10:07:49 +0900

    Refactor : 헤더 레이아웃 수정, 아이콘 모두 빼고 인풋만 남김 position-relative 부여

--- src/componets/home/header/Header.js
+++ src/componets/home/header/Header.js
@@ -1,14 +1,10 @@
-import React, { useContext, useState } from 'react';
-import { Badge, Button, Form } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import { Form } from 'react-bootstrap';
 import HamburgerBtn from './HambergurBtn';
-import { Link } from 'react-router-dom';
 import { ShoppingCartContext } from '../../../App';
 import SearchField from '../search/SearchField';
-import Category from '../category/Category';
 
 function Header() {
-
-    const { cart } = useContext(ShoppingCartContext);
 
     //검색창 관련
     const [input, setInput] = useState('');
@@ -19,11 +15,6 @@ function Header() {
 
     const handleInput = (e) => {
         const value = e.target.value.trim();
-
-        //값이 있으면 클리어버튼 보여주기
-        if(value.length > 0){
-            setClearBtn(true);
-        } 
         setInput(value);
     }
 
@@ -33,50 +24,41 @@ function Header() {
         setClearBtn(false);
     }
 
-    return (
-        <div className='sticky-top bg-white '>
-            <header>
-                <div className="py-2 d-flex border-bottom">
+    //input에 따라 클리어 버튼 보여줄지 
+    useEffect(() => {
+        if (input.length > 0) {
+            setClearBtn(true);
+        } else {
+            setClearBtn(false);
+        }
+    }, [input]);
 
-                    {/* 햄버거 버튼 */}
-                    <HamburgerBtn></HamburgerBtn>
+    return (
+        <header>
+
+            {/* 첫째줄 햄버거버튼,검색창 */}
+            <div className='position-relative bg-white z-2'>
+                <div className="p-3 pb-2">
                     {/* 검색창 */}
                     <div className='flex-grow-1 position-relative d-flex align-items-center' >
-                        <Form.Control size="lg" id='searchBar' type="text" className='pe-5' placeholder="🔍 검색하기" onInput={handleInput} value={input} onClick={() => setShow(true)} />
+                        {/* <img src={process.env.PUBLIC_URL + '/logo.png'} className='position-absolute ms-3' style={{width:'35px'}}/> */}
+                        <Form.Control size="lg" id='searchBar' type="text" className='ps-4 pe-5 rounded-5 border-2 border-success-subtle' placeholder="🔍 검색하기" onInput={handleInput} value={input} onClick={() => setShow(true)} />
+                        {/* 클리어버튼 */}
                         {clearBtn &&
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-x  position-absolute end-0 me-2 h-100" viewBox="0 0 16 16" onClick={handleClear}>
-                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                        </svg>
+                            <div className='position-absolute px-2 me-2 end-0 h-75' onClick={handleClear}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-x my-auto h-100" viewBox="0 0 16 16">
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                                </svg>
+                            </div>
                         }
                     </div>
-
-                    {/* 장바구니 아이콘 */}
-                    <Link to="/shopping-cart" replace={true}>
-                        <Button variant="default" className='position-relative'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-cart2" viewBox="0 0 16 16">
-                                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l1.25 5h8.22l1.25-5zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0" />
-                            </svg>
-
-
-                            {/* 수량 벳지 */}
-                            {cart.length !== 0 &&
-                                <div className='z-1 position-absolute top-0 start-50'>
-                                    <Badge bg="danger" className='rounded-circle'>{cart.length}</Badge>
-                                </div>
-                            }
-                        </Button>
-                    </Link>
                 </div>
+            </div>
 
-                <div className='position-relative'>
-                    {/* 연관검색어 */}
-                    <SearchField input={input} setShow={setShow} show={show}></SearchField>
-                    {/* 카테고리 */}
-                    <Category></Category>
-                </div>
-            </header>
+            {/* 연관검색어 */}
+            <SearchField input={input} setInput={setInput} setShow={setShow} show={show}></SearchField>
 
-        </div>
+        </header>
     );
 }
 
