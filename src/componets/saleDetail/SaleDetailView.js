1be7b43ae2d313dc048536601cc0704caeee504a
jihyuuk_home <jihyuk.dev@gmail.com>
2024-02-16 06:44:32 +0900

    HTML/CSS : 판매상세보기에 인쇄하기 버튼 추가함, 기능 구현은 안함

--- src/componets/saleDetail/SaleDetailView.js
+++ src/componets/saleDetail/SaleDetailView.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { TokenContext, TotalPrice } from '../../App';
 import axios from 'axios';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 import SubHeader from '../subHeader/SubHeader';
 
 function SaleDetailView() {
@@ -144,8 +144,17 @@ function SaleDetailView() {
                     {/* 여기 수정 필요 */}
                     <div className='ps-1'>{sale.pay === 'card' ? '카드' : '현금'}</div>
                 </ListGroupItem>
-
             </ListGroup>
+            <div className='text-center py-4 bg-white'>
+                <Button variant='outline-success' className='fw-semibold fs-5 py-2 px-5 rounded-5'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-printer me-2" viewBox="0 0 16 16">
+                        <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1" />
+                        <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1" />
+                    </svg>
+                    인쇄하기
+                </Button>
+            </div>
+
         </div>
     );
 }
