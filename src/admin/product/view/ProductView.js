9338b081ff1d8b96db0482b592973eb4d7004e6d
jihyuuk_home <jihyuk.dev@gmail.com>
2024-02-25 09:41:08 +0900

    #3 Feat : 상품추가 페이지 레이아웃 구현

---
+++ src/admin/product/view/ProductView.js
@@ -0,0 +1,197 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Form, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { useInitData } from '../../../custom/provider/InitDataContext';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import ServerApi from '../../../server/ServerApi';
+import { useToken } from '../../../custom/provider/TokenContext';
+import SubHeader from '../../../componets/common/SubHeader';
+
+function ProductView() {
+
+    const { mojongs, setMojongs, refreshMojongs } = useInitData();
+    const [items, setItems] = useState([]);
+    const [idx, setIdx] = useState();
+
+    //수정,추가 페이
+    const [show, setShow] = useState('');
+    const close = () => setShow('');
+
+    const { token, removeToken, updateToken } = useToken();
+
+    const change = (value) => {
+        setIdx(Number(value));
+        setItems(mojongs[Number(value)].items);
+    }
+
+    //초기화
+    useEffect(() => {
+        if (mojongs && mojongs[0]) {
+            change(0);
+        }
+    }, [])
+
+
+    //드래그 구현
+    const handleDragEnd = (result) => {
+
+        //미리 적용시켜놓고 성공시 아무것도 x 실패시 데이터 다시 불러오기
+
+        // 드래그 앤 드롭이 유효하게 종료된 경우
+        if (!result.destination) return;
+        if (result.destination.index === result.source.index) return;
+
+        //배열복사
+        const copyitems = Array.from(items);
+        //드래그 요소 빼기고 반환값으로 받아옴
+        const [removed] = copyitems.splice(result.source.index, 1);
+        //드래그요소 끼워넣기
+        copyitems.splice(result.destination.index, 0, removed)
+        //적용
+        setItems(copyitems);
+
+
+        //서버 반영
+        ServerApi('put', '/item/seqChange',
+            { 'categoryId': mojongs[idx].id, 'itemIds': copyitems.map(item => item.id) }
+            , token, removeToken, updateToken)
+            .then(response => {
+                //성공
+                console.log('성공');
+                //데이터 다시 요청
+                refreshMojongs();
+            })
+            .catch(error => {
+                //에러처리
+                alert("요청 실패, 관리자에게 문의하세요")
+                console.error(error);
+                //데이터 다시 요청
+                refreshMojongs();
+            })
+    }
+
+
+    return (
+        <>
+            <div className='my-content px-2'>
+
+                {/* select 영역 */}
+                <div className='bg-white mt-2 mb-1'>
+                    <Form.Select className='fs-5 text-success fw-semibold' onChange={(e) => change(e.target.value)}>
+                        {mojongs.map((mojong, index) => (
+                            <option key={index} value={index}>{mojong.name}</option>
+                        ))}
+                    </Form.Select>
+                </div>
+
+                {/* 총 n개 */}
+                <div className='p-2 fw-midium text-secondary'>총 <span className='fw-semibold'>{items.length}</span>개</div>
+
+
+                {/* 아이템 영역 */}
+                <ListGroup>
+
+                    {/* 1 */}
+                    <DragDropContext onDragEnd={handleDragEnd}>
+                        {/* 2 */}
+                        <Droppable droppableId="droppable">
+                            {(provided) => (
+                                <div ref={provided.innerRef} {...provided.droppableProps}>
+
+                                    {/* 3 */}
+                                    {items.map((item, index) => (
+
+                                        <Draggable key={index} draggableId={`draggable-${index}`} index={index}>
+
+                                            {(provided) => (
+                                                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+
+                                                    <ListGroupItem key={index} className='pe-0 rounded-3'>
+                                                        <div className='d-flex'>
+                                                            <div className='flex-grow-1'>
+                                                                <div className='fw-semibold fs-5'>{item.name}</div>
+                                                                <div className='fw-medium text-secondary'>{item.description}</div>
+                                                            </div>
+
+                                                            <div className='p-3'>
+                                                                <span className='me-3 fs-5'>{item.price}원</span>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-pencil-square text-success" viewBox="0 0 16 16">
+                                                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                                                </svg>
+                                                            </div>
+                                                        </div>
+                                                    </ListGroupItem>
+
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
+                </ListGroup>
+
+            </div>
+
+
+            {/* 하단버튼 */}
+            <div className='text-center p-2 border-top bg-white' onClick={() => setShow('create')}>
+                <Button variant='success' className='w-100 fs-5 fw-semibold p-2 rounded-3'>
+                    상품 추가하기
+                </Button>
+            </div>
+
+            {/* 추가,수정 페이지 */}
+            {show === 'create' &&
+                <div className='position-absolute w-100 h-100 z-5'>
+
+                    <div className='my-container'>
+
+                        <SubHeader value='상품추가' to='/'></SubHeader>
+
+                        <div className='my-content p-3'>
+                            <Form>
+                                {/* 카테고리 */}
+                                <Form.Group className="mb-3">
+                                    <Form.Label className='fs-5 fw-medium text-success'>카테고리</Form.Label>
+                                    <Form.Select size="lg">
+                                        {mojongs.map((mojong, index) => (
+                                            <option key={index} value={index}>{mojong.name}</option>
+                                        ))}
+                                    </Form.Select>
+                                </Form.Group>
+
+                                {/* 이름 */}
+                                <Form.Group className="mb-3">
+                                    <Form.Label className='fs-5 fw-medium text-success'>상품명</Form.Label>
+                                    <Form.Control size="lg" type="text" placeholder='ex) 상품A' />
+                                </Form.Group>
+
+                                {/* 설명 */}
+                                <Form.Group className="mb-3">
+                                    <Form.Label className='fs-5 fw-medium text-success'>설명란</Form.Label>
+                                    <Form.Control size="lg" type="text" placeholder='ex) 50구 / 청색판' />
+                                </Form.Group>
+
+                                {/* 가격 */}
+                                <Form.Group className="mb-3">
+                                    <Form.Label className='fs-5 fw-medium text-success'>가격</Form.Label>
+                                    <Form.Control size="lg" type="text" placeholder='ex) 500원' />
+                                </Form.Group>
+                            </Form>
+                        </div>
+
+                        <div className='mt-auto w-100 bg-white p-2'>
+                            <Button variant='success' className='w-100'>추가하기</Button>
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
+    );
+}
+
+export default ProductView;
