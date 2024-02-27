b3d31936e17f08c5c9cd8e6a47638888680f4e1e
jihyuuk_home <jihyuk.dev@gmail.com>
2024-02-27 17:46:35 +0900

    #3 HTML/CSS : 상품리스트 가격 빠진거 수정함

--- src/admin/product/view/ProductView.js
+++ src/admin/product/view/ProductView.js
@@ -7,6 +7,7 @@ import { useToken } from '../../../custom/provider/TokenContext';
 import SubHeader from '../../../componets/common/SubHeader';
 import CreateProduct from '../productModals/CreateProduct';
 import DeleteProduct from '../productModals/DeleteProduct';
+import EditProduct from '../productModals/EditProduct';
 
 function ProductView() {
 
@@ -129,6 +130,9 @@ function ProductView() {
                                                             </div>
 
                                                             <div>
+                                                                <span className='me-2 fs-5 fw-medium'>
+                                                                    {item.price.toLocaleString('ko-KR')}원
+                                                                </span>
                                                                 <span className='p-2' >
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-pencil-square text-success" viewBox="0 0 16 16">
                                                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -170,6 +174,7 @@ function ProductView() {
             {/* 추가 페이지 */}
             {show === 'create' && <CreateProduct handleClose={handleClose} />}
             {show === 'delete' && <DeleteProduct mojong={selected} modal={{ show, handleClose }} />}
+            {show === 'edit' && <EditProduct selected={selected} handleClose={handleClose}/>}
         </>
     );
 }
