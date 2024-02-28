91dfcb3758c4fb18daa037d2e2fe24a58822de27
jihyuuk_home <jihyuk.dev@gmail.com>
2024-02-28 09:11:03 +0900

    #3 Feat : 상품 수정 기능 구현 완료

--- src/admin/product/view/ProductView.js
+++ src/admin/product/view/ProductView.js
@@ -20,7 +20,7 @@ function ProductView() {
 
     //수정 버튼 클릭시
     const editClick = (index) => {
-        setSelected(items[index]);
+        setSelected({item:items[index], categoryId : mojongs[idx].id});
         setShow('edit');
     }
 
@@ -133,7 +133,7 @@ function ProductView() {
                                                                 <span className='me-2 fs-5 fw-medium'>
                                                                     {item.price.toLocaleString('ko-KR')}원
                                                                 </span>
-                                                                <span className='p-2' >
+                                                                <span className='p-2'  onClick={() =>  editClick(index) }>
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-pencil-square text-success" viewBox="0 0 16 16">
                                                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                                                         <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
