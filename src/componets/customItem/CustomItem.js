1b27bbec3b626c590e29e01a668859b5ac75f924
jihyuuk_home <jihyuk.dev@gmail.com>
2024-03-21 03:23:25 +0900

    Refactor : 아이템 추가 레이아웃 수정

--- src/componets/customItem/CustomItem.js
+++ src/componets/customItem/CustomItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import SubHeader from '../common/SubHeader'
 import { useNavigate } from 'react-router-dom';
@@ -12,6 +12,11 @@ function CustomItem() {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
     const [quantity, setQuantity] = useState('');
+    const [total, setTotal] = useState(0);
+
+    useEffect(()=>{
+        setTotal(price*quantity);
+    },[price, quantity]);
 
     //검증
     const [isInValidName, setIsInValidName] = useState(false);
@@ -44,34 +49,34 @@ function CustomItem() {
             }
             return;
         }
-        if(quantity <= 0){
+        if (quantity <= 0) {
             setFeedbackQuantity('수량은 0 보다 커야합니다.')
             setIsInValidQuantity(true);
         }
 
         //장바구니에 담기
         addCart(name, price, quantity)
-        &&
-        //성공시 리다이렉트
-        navigate('/shopping-cart', { replace: true });
+            &&
+            //성공시 리다이렉트
+            navigate('/shopping-cart', { replace: true });
     }
 
     const nameChange = (value) => {
         setName(value);
-        if(isInValidName) setIsInValidName(false);
+        if (isInValidName) setIsInValidName(false);
     }
     const priceChange = (value) => {
         console.log(value)
         setPrice(value);
-        if(isInValidPrice) setIsInValidPrice(false);
+        if (isInValidPrice) setIsInValidPrice(false);
     }
     const quantityChange = (value) => {
-        if(value === 0){
+        if (value === 0) {
             setQuantity('')
-        }else{
+        } else {
             setQuantity(value);
         }
-        if(isInValidQuantity) setIsInValidQuantity(false);
+        if (isInValidQuantity) setIsInValidQuantity(false);
     }
 
     return (
@@ -79,51 +84,47 @@ function CustomItem() {
 
             <SubHeader value='직접 입력하기' to='/shopping-cart'></SubHeader>
 
-            <Container>
-                <Row className='mt-3'>
-                    <Col>
-                        <Form.Label className='fs-5 fw-medium text-success'>상품명</Form.Label>
-                        <Form.Control type="text" className='py-2' placeholder="상품명을 입력해주세요" value={name} onChange={(e) => nameChange(e.target.value)} isInvalid={isInValidName} />
-                        <Form.Control.Feedback type="invalid" className='text-start'>{feedbackName}</Form.Control.Feedback>
-                    </Col>
-                </Row>
+            <div className='my-content p-3'>
 
-                <Row className='mt-1'>
-                    <Col className='mt-2'>
-                        <span className='rounded-pill border px-2 py-1 me-2' onClick={() => { nameChange('상품A') }}>
-                            상품A
-                        </span>
-                        <span className='rounded-pill border px-2 py-1 me-2' onClick={() => { nameChange('상품B') }}>
-                            상품B
-                        </span>
-                        <span className='rounded-pill border px-2 py-1 me-2' onClick={() => { nameChange('상품C') }}>
-                            상품C
-                        </span>
-                        <span className='rounded-pill border px-2 py-1 me-2' onClick={() => { nameChange('상품D') }}>
-                            상품D
-                        </span>
-                    </Col>
-                </Row>
+                <div>
+                    <Form.Label className='fs-5 fw-medium text-success'>상품명</Form.Label>
+                    <Form.Control type="text" className='py-2' placeholder="상품명을 입력해주세요" value={name} onChange={(e) => nameChange(e.target.value)} isInvalid={isInValidName} />
+                    <Form.Control.Feedback type="invalid" className='text-start'>{feedbackName}</Form.Control.Feedback>
+                </div>
 
-                <Row className='mt-3'>
-                    <Col className=''>
-                        <Form.Label className='fs-5 fw-medium  text-success'>단가</Form.Label>
-                        <Form.Control type="number" className='py-2' placeholder="0원"  pattern="\d*" min="0" inputMode="numeric" value={price} onChange={(e) => priceChange(Number(e.target.value))} isInvalid={isInValidPrice} />
-                        <Form.Control.Feedback type="invalid" className='text-start'>{feedbackPrice}</Form.Control.Feedback>
-                    </Col>
-                    <Col>
-                        <Form.Label className='fs-5 fw-medium  text-success'>수량</Form.Label>
-                        <Form.Control type="number" className='py-2' placeholder="0개"  pattern="\d*" min="0" inputMode="numeric" value={quantity} onChange={(e) => quantityChange(Number(e.target.value))} isInvalid={isInValidQuantity} />
-                        <Form.Control.Feedback type="invalid" className='text-start'>{feedbackQuantity}</Form.Control.Feedback>
-                    </Col>
-                </Row>
+                <div className='mt-2'>
+                    <span className='rounded-pill border px-3 py-1 me-2 bg-success-subtle' onClick={() => { nameChange('상품A') }}>
+                        농자재
+                    </span>
+                    <span className='rounded-pill border px-3 py-1 me-2 bg-success-subtle' onClick={() => { nameChange('상품B') }}>
+                        농기구
+                    </span>
+                    <span className='rounded-pill border px-3 py-1 me-2 bg-success-subtle' onClick={() => { nameChange('상품C') }}>
+                        씨앗
+                    </span>
+                    <span className='rounded-pill border px-3 py-1 me-2 bg-success-subtle' onClick={() => { nameChange('상품D') }}>
+                        호스
+                    </span>
+                </div>
 
-                <Row className='mt-4'>
-                    <Col className='text-center'>
-                        <Button variant='success rounded-4 w-100 py-2 fw-semibold' disabled={false} onClick={() => submit()}>추가하기</Button>
-                    </Col>
-                </Row>
-            </Container>
+                <div className='mt-3'>
+                    <Form.Label className='fs-5 fw-medium  text-success'>수량</Form.Label>
+                    <Form.Control type="number" className='py-2' placeholder="0개" pattern="\d*" min="0" inputMode="numeric" value={quantity} onChange={(e) => quantityChange(Number(e.target.value))} isInvalid={isInValidQuantity} />
+                    <Form.Control.Feedback type="invalid" className='text-start'>{feedbackQuantity}</Form.Control.Feedback>
+                </div>
+
+                <div className='mt-3'>
+                    <Form.Label className='fs-5 fw-medium  text-success'>단가</Form.Label>
+                    <Form.Control type="number" className='py-2' placeholder="0원" pattern="\d*" min="0" inputMode="numeric" value={price} onChange={(e) => priceChange(Number(e.target.value))} isInvalid={isInValidPrice} />
+                    <Form.Control.Feedback type="invalid" className='text-start'>{feedbackPrice}</Form.Control.Feedback>
+                </div>
+            </div>
+
+            <div className='p-2 border-top bg-white'>
+                <Button variant='success fs-5 fw-semibold p-2 rounded-3 w-100' disabled={total <= 0} onClick={() => submit()}>
+                    {(total).toLocaleString('ko-KR')}원 · 추가하기
+                </Button>
+            </div>
 
         </div>
     );
